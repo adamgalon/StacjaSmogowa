ddffeec0c4a5280dd386372cda961236517134ed
adamgalon <adam0galon@gmail.com>
2022-04-13 12:48:00 +0100

    Test1

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@
 #include "WiFi.h"
 #include "FirebaseESP32.h"
 
-#define FIREBASE_HOST ""
-#define FIREBASE_AUTH ""
+#define FIREBASE_HOST "https://aplikacjaiotinzynier-39c1d.firebaseio.com/"
+#define FIREBASE_AUTH "AIzaSyALLMnhKy2GEaJySmnA-_dGOZSezZfh7rk"
 
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "VM3509513"
+#define WIFI_PASSWORD "ftMtsngh3phw"
 
 #define RXD2 25
 #define TXD2 26
@@ -33,23 +33,33 @@ PMS::DATA data;
 PMS pms(SerialPMS);
 Adafruit_BME280 bme;
 
-void initWiFi();
-void readDataFromSensors();
-void sendDataToDb();
-
 void setup()
 {
   Serial.begin(9600);
   SerialPMS.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  initWiFi();
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(400);
+  }
+
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("jestem tu");
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  Serial.println("jestem po 1111");
+
+  Firebase.setReadTimeout(firebaseData, 1000 * 60);
+  Firebase.setwriteSizeLimit(firebaseData, "tiny");
 }
 
 void loop()
-{
-  readDataFromSensors();
-}
-
-void readDataFromSensors()
 {
   if (pms.read(data) && bme.begin(0x76))
   {
@@ -102,41 +112,12 @@ void readDataFromSensors()
     }
 
     Serial.println();
-    sendDataToDb();
-    delay(5000);
+    Firebase.setDouble(firebaseData, path + "/pmsData1", dbPms1);
+    Firebase.setDouble(firebaseData, path + "/pmsData2", dbPms2);
+    Firebase.setDouble(firebaseData, path + "/pmsData10", dbPms10);
+
+    Firebase.setFloat(firebaseData, path + "/humidity", dbHumidity);
+    Firebase.setFloat(firebaseData, path + "/pressure", dbPressure / 100.0F);
+    Firebase.setFloat(firebaseData, path + "/temperature", dbTemperature);
   }
-}
-
-void sendDataToDb()
-{
-  Firebase.setDouble(firebaseData, path + "/pmsData1", dbPms1);
-  Firebase.setDouble(firebaseData, path + "/pmsData2", dbPms2);
-  Firebase.setDouble(firebaseData, path + "/pmsData10", dbPms10);
-
-  Firebase.setFloat(firebaseData, path + "/humidity", dbHumidity);
-  Firebase.setFloat(firebaseData, path + "/pressure", dbPressure / 100.0F);
-  Firebase.setFloat(firebaseData, path + "/temperature", dbTemperature);
-}
-
-void initWiFi()
-{
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.reconnectWiFi(true);
-
-  Firebase.setReadTimeout(firebaseData, 1000 * 60);
-  Firebase.setwriteSizeLimit(firebaseData, "tiny");
 }
